35e14ba6449c92c8a0a4291d1c205838d71bffb5
Ketan Vishwakarma <ketanvishwakarma03@gmail.com>
2021-05-04 11:56:44 +0530

    fade transition

--- lib/widget_fade_transition.dart
+++ lib/widget_fade_transition.dart
@@ -1,36 +1,35 @@
+/*
+* Used to make simple opacity transition animation
+* require :
+*     class defined with SingleTickerProviderStateMixin
+*     opacity : it's define visibility of the widget so it's should be in the changing mode so it will going to change opacity from 0 to high.
+*             we ues curved animation and it's parent uses animation controller;
+*             now animation controller uses SingleTickerProviderStateMixin for it's attribute 'vsync' : we set to 'this',
+*                   and duration.
+*             animation controller should be define in the variable because it's must be set  _controller.forward(); when app is running.
+*
+*     child: the widget we going to use
+*
+*/
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/link.dart';
 
 import 'main.dart';
 
-
-class MadeFadeIn extends StatefulWidget {
-  final Widget child;
-
-  const MadeFadeIn({Key key, this.child}) : super(key: key);
-
+class WidgetFadeTransition extends StatefulWidget {
   @override
-  _MadeFadeInState createState() => _MadeFadeInState();
+  _WidgetFadeTransitionState createState() => _WidgetFadeTransitionState();
 }
 
-class _MadeFadeInState extends State<MadeFadeIn>
+class _WidgetFadeTransitionState extends State<WidgetFadeTransition>
     with SingleTickerProviderStateMixin {
 
   AnimationController _controller;
-  Animation _animation;
 
   @override
   void initState() {
-    _controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 3));
-    /*_animation = Tween(
-        begin: 0.0,
-        end: 6.0
-    ).animate(_controller);*/
-    _animation = CurvedAnimation(
-      curve: Curves.easeInExpo,
-      parent: _controller,
-    );
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 3000));
     super.initState();
   }
 
@@ -43,24 +42,40 @@ class _MadeFadeInState extends State<MadeFadeIn>
   @override
   Widget build(BuildContext context) {
     _controller.forward();
-    return FadeTransition(opacity: _animation, child: widget.child,);
-  }
-}
-
-
-class WidgetFadeTransition extends StatefulWidget {
-  @override
-  _WidgetFadeTransitionState createState() => _WidgetFadeTransitionState();
-}
-
-class _WidgetFadeTransitionState extends State<WidgetFadeTransition> {
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("FadeTransition Widget")),
         body: Container(
-          child: MadeFadeIn(child: Center(child: Text("Ketan Vishwakarma")),),
+          padding: EdgeInsets.all(20),
+          child: Wrap(
+            spacing: 20,
+            children: [
+              FadeTransition(
+                opacity:
+                    CurvedAnimation(parent: _controller, curve: Curves.easeInExpo),
+                child: Text('Curves.easeInExpo',),
+              ),
+              FadeTransition(
+                opacity: CurvedAnimation(
+                    parent: _controller, curve: Curves.easeIn),
+                child: Text('Curves.easeIn'),
+              ),
+              FadeTransition(
+                opacity:
+                CurvedAnimation(parent: _controller, curve: Curves.linear),
+                child: Text('Curves.linear'),
+              ),
+              FadeTransition(
+                opacity: CurvedAnimation(
+                    parent: _controller, curve: Curves.easeInCirc),
+                child: Text('Curves.easeInCirc'),
+              ),
+              FadeTransition(
+                opacity: CurvedAnimation(
+                    parent: _controller, curve: Curves.easeInCirc),
+                child: Text('Curves.easeInCirc'),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: Link(
             target: LinkTarget.self,
